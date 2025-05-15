d67853dc321e81ef0c2ccdc15aa7a92755f79ff4
jenit-tech <jenitjosephjose@gmail.com>
2025-05-15 16:29:19 +0530

    changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Rubik, Manrope } from "@next/font/google";
+import { Rubik, Manrope } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
